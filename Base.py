f8188a8046f990c22b4777602befda9a3052426e
MrRGTF <39508942+MrRGTF@users.noreply.github.com>
2018-05-22 17:32:36 +0800

    Update Base.py

--- Base.py
+++ Base.py
@@ -63,85 +63,143 @@ def initiateTableGrid(tableGrid, tableSize):
 
 
 #------------------Win Conditions------------------#
-def checkWin(tableGrid, player, tablelength):
-    if checkHorizontal(tableGrid, player) == True or checkVertical(tableGrid, player, 0, 0, tablelength) == True:
+def checkWin(tableGrid, player, tableLength, x, y):
+    if checkHorizontal(tableGrid, player, x, y, tableLength) or checkVertical(tableGrid, player, x, y, tableLength) or checkDiagonal(tableGrid, player, x-1, y-1, tableLength):
         return True
 
-def checkHorizontal(tableGrid, player):
-    for row in tableGrid:
-        for cIndex, column in enumerate(row):
-            if cIndex < 6:
-                count = 0
-                for number in range(0,5):
-                    if row[cIndex] == player:
-                        count += 1
-                        cIndex += 1
-                    else:
-                        break
-                if count > 4:
-                    return True
+def checkHorizontal(tableGrid, player, x, y, tableLength):
+    count = 0
+    for number in range(0, tableLength):
+        if tableGrid[y-1][number] == player:
+            count += 1
+        else:
+            count = 0
+        if count == 5:
+            return True
     return False
                 
-def checkVertical(tableGrid, player, x, y, tablelength):
-    for column in range(x, tablelength):
-        for row in range(y, tablelength):
+def checkVertical(tableGrid, player, x, y, tableLength):
+    count = 0
+    for number in range(0, tableLength):
+        if tableGrid[number][x-1] == player:
+            count += 1
+        else:
             count = 0
-            if tableGrid[row][column] == player and count < 5:
-                count += 1
-                checkVertical(tableGrid, player, x, y + 1, tablelength)
-            elif tableGrid[row][column] != player:
-                count = 0
-            elif tableGrid[row][column] == player and count == 4:
-                count = 5
-                return True
-            else: return False
+        if count == 5:
+            return True
+    return False
 
-#def checkDiagonal(tableGrid, player):
-    
+def checkDiagonal(tableGrid, player, x, y, tableLength):
+    if checkDiagonal1(tableGrid, player, x, y, tableLength) or checkDiagonal2(tableGrid, player, x, y, tableLength):
+        return True
+    else:
+        return False
+
+def checkDiagonal1(tableGrid, player, x, y, tableLength):
+    while True:
+        if (x == 0) or (y == 9):
+            iniX = x
+            iniY = y
+            break
+        else:
+            x -= 1
+            y += 1
+
+    count = 0
+    for number in range(0, tableLength):
+        try:
+            if tableGrid[iniY-number][iniX+number] == player:
+                count += 1
+            else:
+                count = 0
+            if count == 5:
+                return True
+
+        except:
+            break
+
+    return False
+
+def checkDiagonal2(tableGrid, player, x, y, tableLength):
+    while True:
+        if (x == 0) or (y == 0):
+            iniX = x
+            iniY = y
+            break
+        else:
+            x -= 1
+            y -= 1
+
+    count = 0
+    for number in range(0, tableLength):
+        try:
+            if tableGrid[iniY+number][iniX+number] == player:
+                count += 1
+            else:
+                count = 0
+            if count == 5:
+                return True
+        except:
+            break
+
+    return False
 
 #========================Code========================#
 
 drawField()
 initiateTableGrid(tableGrid, 10)
-
-for i in range (1, 102, 1):
-    print(tableGrid)
-    if i % 2 == 1:
-    
-        while True:
-            x = int(input("Player 1, input x: "))
-            y = int(input("Player 1, input y: "))
-            if x < 1 or x > 10:
-                print("Not valid")
-            elif y < 1 or y > 10:
-                print("Not valid")
-            elif tableGrid[y-1][x-1] != 0:
-                print("You can't place a piece here!")
-            else:
-                break  
-        moveP1(x, y, i)
-        tableGrid[y-1][x-1] = 1
-        if checkWin(tableGrid, 1, 10) == True:
-            print("Player 1 won")
-            finish = input("Press enter to exit")
-            sys.exit(1)
-    elif i % 2 == 0:
-        while True:
-            x = int(input("Player 2, input x: "))
-            y = int(input("Player 2, input y: "))
-            if x < 1 or x > 10:
-                print("Not valid")
-            elif y < 1 or y > 10:
-                print("Not valid")
-            elif tableGrid[y-1][x-1] != 0:
-                print("You can't place a piece here!")
-            else:
-                break
-        moveP2(x, y, i)
-        tableGrid[y-1][x-1] = 2
-        if checkWin(tableGrid, 2, 10) == True:
-            print("Player 2 won")
-            finish = input("Press enter to exit")
-            sys.exit(1)
+def humanVSHuman():
+    for i in range (1, 102, 1):
+        if i % 2 == 1:
+        
+            while True:
+                x = int(input("Player 1, input x: "))
+                y = int(input("Player 1, input y: "))
+                if x < 1 or x > 10 or y < 1 or y > 10:
+                    print("Not valid")
+                elif tableGrid[y-1][x-1] != 0:
+                    print("You can't place a piece here!")
+                else:
+                    break
+                
+            moveP1(x, y, i)
+            tableGrid[y-1][x-1] = 1
+            if checkWin(tableGrid, 1, tableSize, x, y) == True:
+                print("Player 1 won")
+                finish = input("Press enter to exit")
+                sys.exit(1)
+        elif i % 2 == 0:
             
-    else: None
+            while True:
+                x = int(input("Player 2, input x: "))
+                y = int(input("Player 2, input y: "))
+                if x < 1 or x > 10 or y < 1 or y > 10:
+                    print("Not valid")
+                elif tableGrid[y-1][x-1] != 0:
+                    print("You can't place a piece here!")
+                else:
+                    break
+                
+            moveP2(x, y, i)
+            tableGrid[y-1][x-1] = 2
+            if checkWin(tableGrid, 2, tableSize, x, y) == True:
+                print("Player 2 won")
+                finish = input("Press enter to exit")
+                sys.exit(1)
+
+def humanVSBot():
+    None
+
+def botVSBot():
+    None
+
+while True:
+    menu = input("Input HH or 1 for human vs human, HB or 2 for human vs bot, BB or 3 for bot vs bot: ")
+    if menu == "HH" or menu == 1:
+        humanVSHuman()
+    elif menu == "HB" or menu == 2:
+        humanVSBot()
+    elif menu == "BB" or menu == 3:
+        botVSBot()
+    else: print("That's not a valid input!")
+    
